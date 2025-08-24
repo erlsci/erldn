3a96ea2e97c8a562c678ab0fc497e9ddf4d1cf69
Duncan McGreggor <oubiwann@gmail.com>
2025-08-23 21:50:29 -0500

    Fixed dialyzer error.

--- src/erldn.erl
+++ src/erldn.erl
@@ -7,10 +7,8 @@ parse_str(Str) ->
     case lex_str(Str) of
         {ok, Tokens, _} ->
             case erldn_parser:parse(Tokens) of
-                {ok, Tree}    -> {ok, Tree};
-                {ok, Tree, _Warns} -> {ok, Tree};
-                {error, Error} -> {error, Error, nil};
-                {error, Warns, Errors} -> {error, Errors, Warns}
+                {ok, Tree} -> {ok, Tree};
+                {error, Error} -> {error, Error, nil}
             end;
         Error -> Error
     end.
